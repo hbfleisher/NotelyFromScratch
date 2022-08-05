bd23f83a094e930f10e63ed6cd11bf00cc3a8496
hbfleisher <harrisonbfleisher@gmail.com>
2022-08-04 20:19:59 -0400

    Add project files.

---
+++ NotelyFromScratch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NotelyFromScratch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
