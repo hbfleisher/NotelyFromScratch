b8a6ad24df64503259fc9eda9d9c2e56d28386a3
hbfleish <hbfleish@bju.edu>
2022-08-05 17:43:14 -0400

    updates to HomeController

--- NotelyFromScratch/Controllers/HomeController.cs
+++ NotelyFromScratch/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NotelyFromScratch.Models;
 using System.Diagnostics;
+using NotelyFromScratch.Respository;
 
 namespace NotelyFromScratch.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly INoteRepository _noteRepository;
+        public HomeController(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,7 +22,58 @@ namespace NotelyFromScratch.Controllers
 
         public IActionResult Index()
         {
+            var notes = _noteRepository.GetAllNotes().Where(note => note.IsDeleted == false);
+
+            return View(notes);
+        }
+
+        public IActionResult NoteDetail(Guid id)
+        {
+            if (id != Guid.Empty)
+            {
+                var note = _noteRepository.FindNoteById(id);
+                return View(note);
+            }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult NoteEditor(Guid id = default)
+        {
+            if (id != Guid.Empty)
+            {
+                var note = _noteRepository.FindNoteById(id);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NoteEditor(NoteModel noteModel)
+        {
+            var date = DateTime.Now;
+            if (noteModel != null && noteModel.Id != Guid.Empty)
+            {
+                noteModel.Id = Guid.NewGuid();
+                noteModel.CreatedDate = date;
+                noteModel.LastModifiedDate = date;
+                _noteRepository.SaveNote(noteModel);
+            }
+            else
+            {
+                var note = _noteRepository.FindNoteById(noteModel.Id);
+                note.LastModifiedDate = date;
+                note.Subject = noteModel.Subject;
+                note.Detail = noteModel.Detail;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteNote(Guid id)
+        {
+            var note = _noteRepository.FindNoteById(id);
+            note.IsDeleted = true;
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
